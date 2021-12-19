08750d371741559e9c604d952f4694ac755efb4a
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-12-19 22:19:05 +0800

    Experiment: manually handling waves, and scaling speed to reflect waves

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 FASTLED_USING_NAMESPACE
 
 #define DATA_PIN 25
-#define NUM_LEDS (144 * 5)
+#define NUM_LEDS 400
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 
@@ -85,6 +85,17 @@ String params =
     "'default':'0'"
     "},"
     "{"
+    "'name':'pattern_num',"
+    "'label':'Pattern',"
+    "'type':" +
+    String(INPUTRADIO) +
+    ","
+    "'options':["
+    "{'v':'0','l':'Sine Wave'},"
+    "{'v':'1','l':'Random'}],"
+    "'default':'0'"
+    "},"
+    "{"
     "'name':'param1',"
     "'label':'Parameter1',"
     "'type':" +
@@ -101,13 +112,56 @@ String params =
     ","
     "'min':0,'max':255,"
     "'default':'60'"
+    "},"
+    "{"
+    "'name':'param3',"
+    "'label':'Parameter3',"
+    "'type':" +
+    String(INPUTNUMBER) +
+    ","
+    "'min':0,'max':255,"
+    "'default':'60'"
     "}"
     "]";
+
+class RandomChange {
+ public:
+  RandomChange(uint8_t _delta) : RandomChange(_delta, 0, 255) {}
+  RandomChange(uint8_t _delta, uint8_t _min, uint8_t _max) {
+    delta = _delta;
+    min = _min;
+    max = _max;
+  }
+
+  uint8_t get() {
+    if (forward) {
+      c += random8(delta);
+      if (c > max) {
+        c = max;
+        forward = false;
+      }
+    } else {
+      c -= random8(delta);
+      if (c < min) {
+        c = min;
+        forward = true;
+      }
+    }
+
+    return c;
+  }
+
+ private:
+  uint8_t delta, min, max;
+  int16_t c = 0;
+  bool forward = true;
+};
 
 AsyncWebServer server(80);
 AsyncWebConfig conf;
 
 typedef struct params {
+  char num;
   uint16_t fps;
   uint8_t brightness;
   uint32_t base_color;
@@ -116,9 +170,16 @@ typedef struct params {
   int8_t wave_duty;
   uint16_t param1;
   uint16_t param2;
+  uint16_t param3;
 } pattern_config_t;
 
 pattern_config_t pattern;
+
+CRGB leds[NUM_LEDS];
+CRGBPalette16 vary_palette;
+
+RandomChange change_palette(pattern.param1);
+RandomChange change_brightness(pattern.param2, 50, 255);
 
 void readParams() {
   pattern.fps = conf.getInt("fps");
@@ -128,11 +189,17 @@ void readParams() {
   pattern.wave_freq = conf.getFloat("wave_freq");
   pattern.wave_duty = conf.getInt("wave_duty");
 
+  pattern.num = conf.getValue("pattern_num")[0];
+
   pattern.param1 = conf.getInt("param1");
   pattern.param2 = conf.getInt("param2");
+  pattern.param3 = conf.getInt("param3");
+
+  change_palette = RandomChange(pattern.param1);
+  change_brightness = RandomChange(pattern.param2, 100, 255);
 }
 
-void handleRoot(AsyncWebServerRequest *request) {
+void handleRoot(AsyncWebServerRequest* request) {
   conf.handleFormRequest(request);
   if (request->hasParam("SAVE", true)) {
     uint8_t cnt = conf.getCount();
@@ -145,8 +212,6 @@ void handleRoot(AsyncWebServerRequest *request) {
     readParams();
   }
 }
-
-CRGB leds[NUM_LEDS];
 
 void setup() {
   Serial.begin(115200);
@@ -192,22 +257,49 @@ void loop() {
   uint32_t start = micros();
   FastLED.setBrightness(pattern.brightness);
 
-  CRGBPalette16 p = CRGB(pattern.base_color);
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    // triwave8 creates the variations, wave_freq scales frequency (same scaling as how sine works)
-    // delta shifts the pattern
-    uint8_t base = triwave8(pattern.wave_freq * (i + delta));
-    // Shift up/down based on wave_duty. Rescale it to max 255
-    int16_t scaled = (base + pattern.wave_duty) * 255 / (255 + pattern.wave_duty);
+  if (pattern.num == '0') {
+    CRGBPalette16 p = CRGB(pattern.base_color);
+    for (uint16_t i = 0; i < NUM_LEDS; i++) {
+      // triwave8 creates the variations, wave_freq scales frequency (same scaling as how sine works)
+      // delta shifts the pattern
+      uint8_t base = triwave8(pattern.wave_freq * (i + delta));
+      // Shift up/down based on wave_duty. Rescale it to max 255
+      int16_t scaled = (base + pattern.wave_duty) * 255 / (255 + pattern.wave_duty);
 
-    uint8_t bri = 0;
-    if (scaled >= 0 && scaled <= 255) {
-      bri = scaled;
-    } else if (scaled > 255) {
-      bri = 255;
+      uint8_t bri = 0;
+      if (scaled >= 0 && scaled <= 255) {
+        bri = scaled;
+      } else if (scaled > 255) {
+        bri = 255;
+      }
+
+      leds[i] = ColorFromPalette(p, i, bri, LINEARBLEND);
+    }
+  } else if (pattern.num == '1') {
+    static bool color = false;
+    static uint8_t last_change = 0;
+
+    for (uint16_t i = 1; i < NUM_LEDS; i++) {
+      leds[i] = leds[i - 1];
+    }
+    leds[0] = leds[1];
+
+    CRGB base_color = CRGB(pattern.base_color);
+    CRGB whiter = base_color - CRGB(50, 50, 50);
+    if (color) {
+      nblend(leds[0], base_color, pattern.param1);
+    } else {
+      nblend(leds[0], whiter, pattern.param1);
     }
 
-    leds[i] = ColorFromPalette(p, i, bri, LINEARBLEND);
+    if (last_change == 0 && random8(pattern.param2) == 0) {
+      color = !color;
+      last_change = pattern.param3;
+    } else if (last_change > 0) {
+      last_change--;
+    }
+
+    // fill_palette(leds, NUM_LEDS, delta, 2, p, 255, LINEARBLEND);
   }
 
   EVERY_N_SECONDS(10) {
@@ -220,6 +312,16 @@ void loop() {
   FastLED.delay(1000 / FPS);
 
   EVERY_N_MILLISECONDS(1000 / CALC_FPS) {
-    delta += pattern.move_speed;
+    static uint8_t delta_sin = 0;
+
+    if (delta_sin < 180) {
+      delta += 1;
+    } else if (delta_sin < 220) {
+      delta += 2;
+    } else {
+      delta += 3;
+    }
+
+    delta_sin += pattern.move_speed;
   }
 }
