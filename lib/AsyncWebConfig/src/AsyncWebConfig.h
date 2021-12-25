b63bece23d5fa3477e6cbf7114724a9535ba0bde
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-12-25 20:39:11 +0800

    Increase max parameters

--- lib/AsyncWebConfig/src/AsyncWebConfig.h
+++ lib/AsyncWebConfig/src/AsyncWebConfig.h
@@ -29,7 +29,7 @@ Dependencies:
 #endif
 */
 //maximum number of parameters
-#define MAXVALUES 20
+#define MAXVALUES 64
 
 //maximum number of options per parameters
 #define MAXOPTIONS 15
@@ -62,8 +62,8 @@ Dependencies:
 #define BTN_CANCEL 2
 #define BTN_DELETE 4
 //data structure to hold the parameter Description
-typedef //Struktur eines Datenpakets
-struct  {
+typedef  //Struktur eines Datenpakets
+    struct {
   char name[NAMELENGTH];
   char label[LABELLENGTH];
   uint8_t type;
@@ -75,7 +75,7 @@ struct  {
 } DESCRIPTION;
 
 class AsyncWebConfig {
-public:
+ public:
   AsyncWebConfig();
   //load form descriptions
   void setDescription(String parameter);
@@ -83,7 +83,7 @@ public:
   void addDescription(String parameter);
   //function to respond a HTTP request for the form use the filename
   //to save.
-/*
+  /*
 #if defined(ESP32)
   void handleFormRequest(WebServer * server, const char * filename);
   //function to respond a HTTP request for the form use the default file
@@ -99,28 +99,28 @@ public:
 #endif
 */
   void handleFormRequest(AsyncWebServerRequest *request);
-  void handleFormRequest(AsyncWebServerRequest *request, const char * filename);
-  int16_t getIndex(const char * name);
+  void handleFormRequest(AsyncWebServerRequest *request, const char *filename);
+  int16_t getIndex(const char *name);
   //read configuration from file
-  boolean readConfig(const char *  filename);
+  boolean readConfig(const char *filename);
   //read configuration from default file
   boolean readConfig();
   //write configuration to file
-  boolean writeConfig(const char *  filename);
+  boolean writeConfig(const char *filename);
   //write configuration to default file
   boolean writeConfig();
   //delete configuration file
-  boolean deleteConfig(const char *  filename);
+  boolean deleteConfig(const char *filename);
   //delete default configutation file
   boolean deleteConfig();
   //get a parameter value by its name
-  const String getString(const char * name);
-  const char * getValue(const char * name);
-  int getInt(const char * name);
-  float getFloat(const char * name);
-  boolean getBool(const char * name);
+  const String getString(const char *name);
+  const char *getValue(const char *name);
+  int getInt(const char *name);
+  float getFloat(const char *name);
+  boolean getBool(const char *name);
   //get the accesspoint name
-  const char * getApName();
+  const char *getApName();
   //get the number of parameters
   uint8_t getCount();
   //get the name of a parameter
@@ -130,21 +130,21 @@ public:
   //Ser values from a JSON string
   void setValues(String json);
   //set the value for a parameter
-  void setValue(const char*name,String value);
+  void setValue(const char *name, String value);
   //set the label for a parameter
-  void setLabel(const char * name, const char* label);
+  void setLabel(const char *name, const char *label);
   //remove all options
   void clearOptions(uint8_t index);
-  void clearOptions(const char * name);
+  void clearOptions(const char *name);
   //add a new option
   void addOption(uint8_t index, String option);
   void addOption(uint8_t index, String option, String label);
   //modify an option
   void setOption(uint8_t index, uint8_t option_index, String option, String label);
-  void setOption(char * name, uint8_t option_index, String option, String label);
+  void setOption(char *name, uint8_t option_index, String option, String label);
   //get the options count
   uint8_t getOptionCount(uint8_t index);
-  uint8_t getOptionCount(char * name);
+  uint8_t getOptionCount(char *name);
   //set form type to doen cancel
   void setButtons(uint8_t buttons);
   //register onSave callback
@@ -158,7 +158,8 @@ public:
 
   //values for the parameter
   String values[MAXVALUES];
-private:
+
+ private:
   char _buf[1000];
   uint8_t _count;
   String _apName;
